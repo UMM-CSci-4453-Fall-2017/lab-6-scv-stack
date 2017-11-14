aff8e3686c9b6265c03838613daad763fde72fe8
scvstackkk <scv.stack@gmail.com>
2017-11-14 02:43:47 -0600

    Got summarize-db.js to the point where everything prints, but could not solve the callback issue in a timely manner. Also, the program doesn't exit correctly, as I could not figure out how to do this.

--- node/summarize-db.js
+++ node/summarize-db.js
@@ -2,7 +2,6 @@
 
 var credentials = require('./credentials.json');
 var mysql = require('mysql');
-var async = require('async');
 
 credentials.host="ids";
 var connection = mysql.createConnection(credentials);
@@ -25,16 +24,19 @@ function printDatabases(connection) {
     if(err){
       console.log("Error: " + err);
     } else {
-      for (var e in rows) {
-        (function(e) {
-	  var database = rows[e].Database;
-          console.log("---|" + database + ">");
-          printTables(database);
-        })(e);
+        var print = function(count) {
+	  if (count < rows.length) {
+	    var database = rows[count].Database;
+	    console.log("---|" + database + ">");
+            printTables(database);
+	    print(count+1);  
+	  }
+	};
+	print(0);
       }
-      }
-  });  
+  }); 
 }
+
 
 function printTables(database) {
   connection.query("SHOW TABLES IN " + database,
@@ -42,39 +44,43 @@ function printTables(database) {
     if(err){
       console.log("Error: " + err);
     } else {
-      var tableString = "rows[e].Tables_in_" + database;
-      for (var e in rows) {
-        (function(e) {
-	  console.log(".....|" + database + "." + 
-          	eval(tableString) + ">");
-          printTableStructure(database, eval(tableString));
-	})(e);
+        var print = function(count) {
+	  if  (count < rows.length) {
+	    var tableString = "rows[count].Tables_in_" + database;
+	    console.log(".....|" + database + "." + 
+          	  eval(tableString) + ">");
+            printTableStructure(database, eval(tableString));
+ 	    print(count+1);
+	  }
+	};
+	print(0);
       }
-    }
   });
 }
 
-function printTableStructure(database, table) {
+function printTableStructure(database, table, count) {
   connection.query("DESCRIBE " + database + "." + table, function(err, rows, fields) {
     if(err){
       console.log("Error: " + err);
     } else {
-	for (var e in rows) {
-          (function(e) {
-	     var field = rows[e].Field;
-	     var type = rows[e].Type;
-	     // 11 spaces default for the padding, unless the field string is greater, then just give it 30 spaces and split from that
-	     var padding = "           ";
- 	     if (field.length > padding.length) {
-	       padding = "                              ";
-	       padding = padding.slice(field.length);
-	     } else {
-	       padding = padding.slice(field.length);
-	       }
-	     console.log("       FieldName: `" + field + "`" + padding + "(" + type + ")");
-          })(e);	    
-        }
-      }	    
+          var print = function(count) {
+	     if (count < rows.length) {
+	       var field = rows[count].Field;
+	       var type = rows[count].Type;
+	       // 11 spaces default for the padding, unless the field string is greater, then just give it 30 spaces and split from that
+	       var padding = "           ";
+ 	       if (field.length > padding.length) {
+	         padding = "                              ";
+	         padding = padding.slice(field.length);
+	       } else {
+	         padding = padding.slice(field.length);
+	         }
+	       console.log("       FieldName: `" + field + "`" + padding + "(" + type + ")");
+	       print(count+1);
+             }
+	  };
+	  print(0);
+      }
   });
 }
 
